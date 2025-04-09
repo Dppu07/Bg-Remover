441805789d7ba1e3b925190de8d7b75620bcbd2d
Darpan Arora <31893535-darpanarora2429@users.noreply.replit.com>
2025-04-09 07:38:03 +0000

    Restored to '1896e8677f8a2fd92ddd452dcff9046b1803d17e'
    Replit-Restored-To: 1896e8677f8a2fd92ddd452dcff9046b1803d17e

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173,
+    host: '0.0.0.0',
   }
 })
