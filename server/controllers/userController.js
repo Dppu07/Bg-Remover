bb72643aebf29afe7fcc9318dcb3d29c95abe920
Darpan Arora <31893535-darpanarora2429@users.noreply.replit.com>
2025-04-10 08:34:00 +0000

    Added Vercel.json

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@
 // API controller function to manage user with database 
 // https://localhost:3000/api/user/webhooks
 
-const userController = async (req, res) =>
+const clerkWebhooks = async (req, res) =>
   {
     
 }
